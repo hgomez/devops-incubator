f747d396a58d0a0488378bf62559436f5d3a4446
Henri Gomez <henri.gomez@gmail.com>
2011-11-28 11:18:08 +0000

    Initial import

---
+++ basic-perf-webapp/src/main/java/org/mycorp/web/performance/DumpServlet.java
@@ -0,0 +1,121 @@
+package org.mycorp.web.performance;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DumpServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><head><title>DumpServlet</title></head>");
+
+        out.println("getAuthType=" + request.getAuthType() + "</br>");
+        out.println("getCharacterEncoding=" + request.getCharacterEncoding() + "</br>");
+        out.println("getContentLength=" + request.getContentLength() + "</br>");
+        out.println("getContextPath=" + filter(request.getContextPath()) + "</br>");
+        out.println("getLocalAddr=" + request.getLocalAddr() + "</br>");
+        out.println("getLocalName=" + request.getLocalName() + "</br>");
+        out.println("getLocalPort=" + request.getLocalPort() + "</br>");
+
+        out.println("getMethod=" + request.getMethod() + "</br>");
+        out.println("getPathInfo=" + filter(request.getPathInfo()) + "</br>");
+        out.println("getPathTranslated=" + filter(request.getPathTranslated()) + "</br>");
+        out.println("getProtocol=" + request.getProtocol() + "</br>");
+        out.println("getRequestURI="+ filter(request.getRequestURI()) + "</br>");
+        out.println("getQueryString=" + filter(request.getQueryString()) + "</br>");
+
+        out.println("getRemoteAddr=" + request.getRemoteAddr() + "</br>");
+        out.println("getRemoteHost=" + request.getRemoteHost() + "</br>");
+        out.println("getRemotePort=" + request.getRemotePort() + "</br>");
+        out.println("getRemoteUser=" + request.getRemoteUser() + "</br>");
+
+        out.println("getRequestedSessionId=" + request.getRequestedSessionId() + "</br>");
+        out.println("getRequestURI=" + request.getRequestURI() + "</br>");
+        out.println("getRequestURL=" + request.getRequestURL() + "</br>");
+        out.println("getScheme=" + request.getScheme() + "</br>");
+        out.println("getServerName=" + request.getServerName() + "</br>");
+        out.println("getServerPort=" + request.getServerPort() + "</br>");
+        out.println("getServletPath=" + request.getServletPath() + "</br>");
+
+        String cipherSuite=
+            (String)request.getAttribute("javax.servlet.request.cipher_suite");
+
+        if (cipherSuite!=null)
+            out.println("javax.servlet.request.cipher_suite=" + request.getAttribute("javax.servlet.request.cipher_suite")+ "</br>");
+
+        Enumeration hnames = request.getHeaderNames();
+
+        if (hnames != null) {
+
+            out.println("headers=</br>");
+
+            while (hnames.hasMoreElements()) {
+                String headername = (String)hnames.nextElement();
+                out.println(headername + ": " + request.getHeader(headername) + "</br>");
+            }
+
+        }
+
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        doGet(request, response);
+    }
+
+
+    /**
+     * Filter the specified message string for characters that are sensitive
+     * in HTML.  This avoids potential attacks caused by including JavaScript
+     * codes in the request URL that is often reported in error messages.
+     *
+     * @param message The message string to be filtered
+     */
+    private String filter(String message) {
+
+        if (message == null)
+            return (null);
+
+        char content[] = new char[message.length()];
+        message.getChars(0, message.length(), content, 0);
+        StringBuilder result = new StringBuilder(content.length + 50);
+        for (int i = 0; i < content.length; i++) {
+            switch (content[i]) {
+            case '<':
+                result.append("&lt;");
+                break;
+            case '>':
+                result.append("&gt;");
+                break;
+            case '&':
+                result.append("&amp;");
+                break;
+            case '"':
+                result.append("&quot;");
+                break;
+            default:
+                result.append(content[i]);
+            }
+        }
+        return (result.toString());
+
+    }
+
+
+}
---
+++ basic-perf-webapp/src/main/java/org/mycorp/web/performance/PerfMeterServlet.java
@@ -0,0 +1,132 @@
+package org.mycorp.web.performance;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/***
+ * A Basic Performance Servlet
+ *
+ * features :
+ *
+ *  * configurable wait-time (in nano-seconds) with waittime parameter (-1 to disable)
+ *  * configurable response-size (in char) with responsesize (-1 to disable)
+ *  * configurable response content (string) with response
+ *
+ * examples :
+ *
+ * mvn clean package
+ *
+ * mvn tomcat:run
+ * mvn jetty:run
+ *
+ * * http://localhost:8080/qa-perf/PerfMeter?waittime=100&responsesize=100000
+ * * http://localhost:8080/qa-perf/PerfMeter?waittime=100&responsesize=-1
+ * * http://localhost:8080/qa-perf/PerfMeter?waittime=100&responsesize=8000&response=exo-platform-rocks
+ *
+ *
+ * @author hgomez
+ *
+ */
+public class PerfMeterServlet extends HttpServlet {
+
+    private long 	defaultWaitTime     = 1000000L;
+    private int  	defaultResponseSize = -1;
+    private String	defaultResponse     = "the quick brown fox jumps over the lazy dog";
+
+    /**
+     * Initialize this servlet.
+     */
+    public void init() throws ServletException {
+
+        String lWaitTimeS     = getServletConfig().getInitParameter("waittime");		// WaitTime in nano seconds
+        String lResponseSizeS = getServletConfig().getInitParameter("responsesize");	// ResponseSize in bytes
+
+        try
+        {
+            if (lWaitTimeS != null)
+                defaultWaitTime = Long.parseLong(lWaitTimeS);
+
+            if (lResponseSizeS != null)
+                defaultResponseSize = Integer.parseInt(lResponseSizeS);
+        }
+        catch (NumberFormatException nfe)
+        {
+        }
+
+        defaultResponse = getServletConfig().getInitParameter("response");				// Response pattern
+    }
+
+    /**
+     * Process a GET request for the specified resource.
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet-specified error occurs
+     */
+    public void doGet(HttpServletRequest pRequest, HttpServletResponse pResponse)
+            throws IOException, ServletException {
+
+        pResponse.setContentType("text/plain");
+
+        PrintWriter lWriter = pResponse.getWriter();
+
+        String lWaitTimeS     = pRequest.getParameter("waittime");		// WaitTime in nano seconds
+        String lResponseSizeS = pRequest.getParameter("responsesize");	// ResponseSize in bytes
+        String lResponseS     = pRequest.getParameter("response");		// ResponseSize in bytes
+
+        long lWaitTime    		= defaultWaitTime;
+        int  lResponseSize 		= defaultResponseSize;
+        String lResponse   		= defaultResponse;
+        int		lResponseLenght = defaultResponse.length();
+
+        try
+        {
+            if (lWaitTimeS != null)
+                lWaitTime = Long.parseLong(lWaitTimeS);
+
+            if (lResponseSizeS != null)
+                lResponseSize = Integer.parseInt(lResponseSizeS);
+        }
+        catch (NumberFormatException nfe)
+        {
+        }
+
+        if (lResponseS != null)
+            lResponse = lResponseS;
+
+        if (lResponse != null)
+            lResponseLenght = lResponse.length();
+
+        if (lWaitTime != -1) {
+
+            try {
+                Thread.sleep(lWaitTime / 1000L, (int)(lWaitTime % 1000L));
+            }
+            catch (InterruptedException ie)
+            {
+
+            }
+        }
+
+        if (lResponseLenght >= 0) {
+            while (lResponseSize >= 0) {
+
+                if (lResponseLenght > lResponseSize)
+                    lWriter.write(lResponse, 0, lResponseSize);
+                else
+                    lWriter.write(lResponse);
+
+                lResponseSize -= lResponseLenght;
+            }
+        }
+
+    }
+
+}
