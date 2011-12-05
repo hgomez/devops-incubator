4a93108b587fe651724e43cb0dee31d468ac4995
Henri Gomez <henri.gomez@gmail.com>
2011-12-05 11:35:26 +0000

    Add GitBlit as Git Repositories hosting solution

---
+++ rpm-packaging/mygitblit/build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+GITBLIT_VERSION=0.7.0
+TOMCAT_VERSION=7.0.23
+
+if [ $# -gt 1 ]; then
+  GITBLIT_VERSION=$1
+  shift
+fi
+
+if [ $# -gt 1 ]; then
+  TOMCAT_VERSION=$1
+  shift
+fi
+
+GITBLIT_URL=http://gitblit.googlecode.com/files/gitblit-${GITBLIT_VERSION}.war
+TOMCAT_URL=http://apache.cict.fr/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+CATALINA_JMX_REMOTE_URL=http://apache.cict.fr/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/extras/catalina-jmx-remote.jar
+
+if [ ! -f SOURCES/glitblit-${GITBLIT_VERSION}.war ]; then
+  echo "downloading glitblit-${GITBLIT_VERSION}.war from $GITBLIT_URL"
+  curl -s -L $GITBLIT_URL -o SOURCES/glitblit-${GITBLIT_VERSION}.war
+fi
+
+if [ ! -f SOURCES/apache-tomcat-${TOMCAT_VERSION}.tar.gz ]; then
+  echo "downloading apache-tomcat-${TOMCAT_VERSION}.tar.gz from $TOMCAT_URL"
+  curl -s -L $TOMCAT_URL -o SOURCES/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+fi
+
+if [ ! -f SOURCES/catalina-jmx-remote-${TOMCAT_VERSION}.jar ]; then
+  echo "downloading catalina-jmx-remote-${TOMCAT_VERSION}.jar from $CATALINA_JMX_REMOTE_URL"
+  curl -s -L $CATALINA_JMX_REMOTE_URL -o SOURCES/catalina-jmx-remote-${TOMCAT_VERSION}.jar
+fi
+
+echo "Version to package is $GITBLIT_VERSION powered by Apache Tomcat $TOMCAT_VERSION"
+
+# prepare fresh directories
+rm -rf BUILD RPMS SRPMS TEMP
+mkdir -p BUILD RPMS SRPMS TEMP
+
+# Build using rpmbuild (use double-quote for define to have shell resolv vars !)
+rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="TOMCAT_REL $TOMCAT_VERSION" --define="GITBLIT_REL $GITBLIT_VERSION"  SPECS/mygitblit.spec
+
