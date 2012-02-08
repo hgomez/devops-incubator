e81f27269a96055454f30d8b3502d6d90036091a
Gomez Henri <henri.gomez@gmail.com>
2012-02-08 16:55:12 +0100

    activate jmx authentication via log/pwd

--- rpm-packaging/myjenkins/SOURCES/setenv.sh
+++ rpm-packaging/myjenkins/SOURCES/setenv.sh
@@ -18,7 +18,7 @@ CATALINA_OPTS=$APP_JAVA_OPTS
 
 JMX_EXT_IP=""
 
-CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true"
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.password.file=$CATALINA_HOME/conf/jmxremote.password -Dcom.sun.management.jmxremote.access.file=$CATALINA_HOME/conf/jmxremote.access"
 
 CATALINA_OPTS="$APP_JAVA_OPTS $CATALINA_OPTS"
