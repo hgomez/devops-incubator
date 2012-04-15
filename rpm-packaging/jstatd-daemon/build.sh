5e8e5ad0f823e111919eecd1b40f863b6a2b4279
Gomez Henri <henri.gomez@gmail.com>
2012-04-15 14:34:04 +0200

    no need for src.rpm production

--- rpm-packaging/jstatd-daemon/build.sh
+++ rpm-packaging/jstatd-daemon/build.sh
@@ -7,5 +7,5 @@ rm -rf BUILD RPMS SRPMS TEMP
 mkdir -p BUILD RPMS SRPMS TEMP
 
 # Build using rpmbuild (use double-quote for define to have shell resolv vars !)
-rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="VERSION $VERSION" SPECS/jstatd-daemon.spec
+rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="VERSION $VERSION" SPECS/jstatd-daemon.spec
 
--- rpm-packaging/myapp/build.sh
+++ rpm-packaging/myapp/build.sh
@@ -28,5 +28,5 @@ rm -rf BUILD RPMS SRPMS TEMP
 mkdir -p BUILD RPMS SRPMS TEMP
 
 # Build using rpmbuild (use double-quote for define to have shell resolv vars !)
-rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="TOMCAT_REL $TOMCAT_VERSION" --define="MYAPP_REL $MYAPP_VERSION"  SPECS/myapp.spec
+rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="TOMCAT_REL $TOMCAT_VERSION" --define="MYAPP_REL $MYAPP_VERSION"  SPECS/myapp.spec
 
