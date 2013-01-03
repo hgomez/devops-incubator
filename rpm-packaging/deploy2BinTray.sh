744d6b4885145c90e65ef634f8916d7aa401d20a
daniel.petisme@gmail.com <daniel.petisme@gmail.com>
2013-01-04 00:52:43 +0100

    BinTray

--- rpm-packaging/deploy2BinTray.sh
+++ rpm-packaging/deploy2BinTray.sh
@@ -95,13 +95,18 @@ function create_package() {
 
 function upload_content() {
   echo "[DEBUG] Uploading ${RPM}..."
-  ${CURL} -T ${RPM} -H X-Bintray-Package:${PCK_NAME} -H X-Bintray-Version:${PCK_VERSION}-${PCK_RELEASE} ${API}/content/${SUBJECT}/${REPO}/${RPM}
+  uploaded=` | $(${CURL} --write-out %{http_code} --silent --output /dev/null -T ${RPM} -H X-Bintray-Package:${PCK_NAME} -H X-Bintray-Version:${PCK_VERSION}-${PCK_RELEASE} ${API}/content/${SUBJECT}/${REPO}/${RPM}) -eq ${CREATED} ] `
+  echo "[DEBUG] RPM ${RPM} uploaded? y:1/N:0 ${package_exists}"
+  return ${uploaded}
 }
 function deploy_rpm() {
   
-  upload_content
-  echo "[DEBUG] Publishing ${RPM}..."
-  ${CURL} -X POST ${API}/content/${SUBJECT}/${REPO}/${PCK_NAME}/${PCK_VERSION}-${PCK_RELEASE}/publish -d "{ \"discard\": \"false\" }"    
+  if ( upload_content); then
+    echo "[DEBUG] Publishing ${RPM}..."
+    ${CURL} -X POST ${API}/content/${SUBJECT}/${REPO}/${PCK_NAME}/${PCK_VERSION}-${PCK_RELEASE}/publish -d "{ \"discard\": \"false\" }"
+  else
+    echo "[SEVERE] First you should upload your rpm ${RPM}"
+  fi    
 }
 
 main "$@"
