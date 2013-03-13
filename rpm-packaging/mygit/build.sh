5d1bca8971daa887e1fd5b34178b815cfe74f555
Gomez Henri <henri.gomez@gmail.com>
2013-03-13 11:56:37 +0100

    No need to produce src.rpm

--- rpm-packaging/mygit/build.sh
+++ rpm-packaging/mygit/build.sh
@@ -5,5 +5,5 @@ rm -rf BUILD RPMS SRPMS TEMP
 mkdir -p BUILD RPMS SRPMS TEMP
 
 # Build using rpmbuild (use double-quote for define to have shell resolv vars !)
-rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="VERSION $RPM_VERSION" SPECS/mygit.spec
+rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" --define="VERSION $RPM_VERSION" SPECS/mygit.spec
 
--- rpm-packaging/mysonar-mysql/build.sh
+++ rpm-packaging/mysonar-mysql/build.sh
@@ -5,5 +5,5 @@ rm -rf BUILD RPMS SRPMS TEMP
 mkdir -p BUILD RPMS SRPMS TEMP
 
 # Build using rpmbuild (use double-quote for define to have shell resolv vars !)
-rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" SPECS/mysonar-mysql.spec
+rpmbuild -bb --define="_topdir $PWD" --define="_tmppath $PWD/TEMP" SPECS/mysonar-mysql.spec
 
