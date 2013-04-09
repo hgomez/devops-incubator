ab65833c0c531a996166bfde9d4351d8e2c129d2
Gomez Henri <henri.gomez@gmail.com>
2013-04-09 22:03:33 +0200

    update golo name

--- rpm-packaging/golo-lang/build.sh
+++ rpm-packaging/golo-lang/build.sh
@@ -6,11 +6,11 @@ VERSION=0-preview1
 rm -rf BUILD RPMS SRPMS TEMP
 mkdir -p BUILD RPMS SRPMS SOURCES TEMP
 
-CRASH_URL=http://sourceforge.net/projects/golo-lang/files/${VERSION}/golo-${VERSION}-distribution.tar.gz/download 
+GOLO_URL=http://sourceforge.net/projects/golo-lang/files/${VERSION}/golo-${VERSION}-distribution.tar.gz/download 
 
 if [ ! -f SOURCES/crash-${VERSION}.tar.gz ]; then
-  echo "downloading crash-${VERSION}.tar.gz from $CRASH_URL"
-  curl -s -L $CRASH_URL -o SOURCES/golo-${VERSION}-distribution.tar.gz
+  echo "downloading golo-${VERSION}.tar.gz from $CRASH_URL"
+  curl -s -L $GOLO_URL -o SOURCES/golo-${VERSION}-distribution.tar.gz
 fi
 
 # Build using rpmbuild (use double-quote for define to have shell resolv vars !)
